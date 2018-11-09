00be0cb95c088f54db99dcb43e1b557475d37537
DONET RODRIGUEZ Joan <joan.donetrodriguez@soprabanking.com>
2018-11-09 20:47:18 +0100

    Second commit: We are able to interact with the player!

--- src/main/java/com/project/joan/App.java
+++ src/main/java/com/project/joan/App.java
@@ -3,15 +3,66 @@ package com.project.joan;
 import java.util.Scanner;
 
 public class App {
-
+	private static final String EXIT = "Q";
+	private static final String GOAHEAD = "W";
+	private static final String TURNLEFT = "A";
+	private static final String TURNRIGHT = "D";
+	private static final String SHOOT = "E";
+	Scanner g_sc = new Scanner(System.in);
+	Board g_board; 
+	Player g_player;
+	
 	public static void main(String[] args) {
-		Scanner l_sc = new Scanner(System.in);
+		App l_app = new App();
+		l_app.createBoard();
+		l_app.createPlayer();
+		l_app.play();
+	}
+	
+	public void createBoard() {
 		System.out.println("WELCOME TO HUNT THE WUMPUS!!");
 		System.out.println("Tell me the dimensions of your square board (min. 4 max. 10): ");
-		int l_choice = l_sc.nextInt();
-		Board b1 = new Board(l_choice);
-		b1.drawBoard();
+		int l_choice = g_sc.nextInt();
+		g_board = new Board(l_choice);
+		g_board.drawBoard();
+	}
+	
+	public void createPlayer() {
+		System.out.println("Tell me the arrows you want to dispose: ");
+		int l_choice = g_sc.nextInt();
+		g_player = new Player(l_choice);
+	}
+	
+	public void play() {
+		System.out.println("Instructions: \n Q-Exit \nW-Go Ahead \nA-Turn left \nD-Turn right \nE-Shoot");
+		System.out.println("Let's start!!");
+		String result = null;
+		String l_entry = "";
+		while (!l_entry.equalsIgnoreCase(EXIT)) {
+			System.out.println("Press a key: ");
+			l_entry = g_sc.nextLine().toUpperCase();
+			switch (l_entry) {
+			case EXIT: l_entry = EXIT; 
+			result = EXIT;
+			break;
+			case GOAHEAD: g_player.goAhead(); 
+			result = GOAHEAD;
+			break;
+			case TURNLEFT: g_player.rotate(TURNLEFT); 
+			result = TURNLEFT;
+			break;
+			case TURNRIGHT: g_player.rotate(TURNLEFT); 
+			result = TURNRIGHT;
+			break;
+			case SHOOT: g_player.shoot(); 
+			result = SHOOT;
+			break;
+			default: result = "NON VALID CHOICE!"; break;
+			}
+			System.out.println("Your choice is: "+result);
+		}
 
+		
 	}
 
 }
--- src/main/java/com/project/joan/Player.java
+++ src/main/java/com/project/joan/Player.java
@@ -2,4 +2,30 @@ package com.project.joan;
 
 public class Player {
 
+	private static final String NORTH = "N";
+	private static final String SOUTH = "S";
+	private static final String EAST = "E";
+	private static final String WEST = "W";
+
+	private int arrows;
+	private String direction;
+	private int[] location;
+	boolean gold;
+	
+	public Player(int p_arr) {
+		arrows = p_arr;
+		direction = EAST;
+	}
+	
+	public void goAhead() {
+		System.out.println("Method goAhead");
+	}
+	
+	public void rotate(String p_dir) {
+		System.out.println("Method rotate");
+	}
+	public void shoot() {
+		System.out.println("Method shoot");
+	}
+	
 }
